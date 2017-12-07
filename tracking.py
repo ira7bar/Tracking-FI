d406b9e782815031e74ce6027c69db30f1df2d78
Ira Bar <ira7bar@gmail.com>
2017-12-07 18:49:09 +0200

    Added patch extraction and patch distance calculation

--- tracking.py
+++ tracking.py
@@ -6,7 +6,13 @@ from matplotlib import patches
 N = 112
 path = "Images/"
 
-
+def read_frames():
+    frames = []
+    for i in range(N):
+        full_path = path + str(i+1).zfill(3) + '.png'
+        f = cv2.imread(full_path)
+        frames.append(f)
+    return frames
 
 def rgb_to_histogram(rgbImage):
     img_q=rgbImage/16
@@ -30,17 +36,39 @@ def rgb_to_histogram(rgbImage):
     return allThree
 
 def hist_dist(p, q):
-
     return np.exp(20*np.sum(np.square(p*q)))
 
-def read_frames():
-    frames = []
-    for i in range(N):
-        full_path = path + str(i+1).zfill(3) + '.png'
-        f = cv2.imread(full_path)
-        frames.append(f)
-    return frames
+
+def patch_dist(I1,I2):
+    p = rgb_to_histogram(I1)
+    q = rgb_to_histogram(I2)
+    return hist_dist(p,q)
+
+
+def get_patch(I,h,w,r_cnt,c_cnt):
+    top = r_cnt - h/2
+    bottom = r_cnt + h/2
+    left = c_cnt - w/2
+    right = c_cnt + w/2
+    patch = I[top:bottom,left:right,:]
+    # print h, w, patch.shape
+    return patch
+
+
 
 
 frames = read_frames()
-rgb_to_histogram(frames[0])
+f = frames[0]
+# plt.imshow(f)
+# plt.show()
+
+p1 = get_patch(f,130,50,134,297)
+p2 = get_patch(f,130,50,136,300)
+p3 = get_patch(f,130,50,290,240)
+
+print patch_dist(p1,p2)
+print patch_dist(p2,p1)
+
+print patch_dist(p1,p3)
+print patch_dist(p2,p3)
+

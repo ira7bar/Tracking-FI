315d20d7056bdcc66d7aa217fe19bee05220b45e
Ira Bar <ira7bar@gmail.com>
2017-12-07 17:12:21 +0200

    reading images

--- tracking.py
+++ tracking.py
@@ -3,4 +3,24 @@ from matplotlib import pylab as plt
 import numpy as np
 from matplotlib import patches
 
+N = 112
+path = "Images/"
 
+
+def normalized_hist(I):
+    pass
+
+
+
+
+def read_frames():
+    frames = []
+    for i in range(N):
+        full_path = path + str(i+1).zfill(3) + '.png'
+        f = plt.imread(full_path)
+        frames.append(f)
+    return frames
+
+
+frames = read_frames()
+normalized_hist(frames[0])
